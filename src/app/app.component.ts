a928f1c2b78aedae1a3a59c50a572ba2d0a4c519
Mulyono Putra <mulyonoputra27@outlook.com>
2022-07-16 21:47:48 +0700

    Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-
+import {
+  Event,
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel,
+} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Clapauious';
+  showLoadingIndicator!: boolean;
+
+  private isPageNotFound!: boolean;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((routeEvent: Event) => {
+      if (routeEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      if (routeEvent instanceof NavigationEnd) {
+        this.showLoadingIndicator = false;
+      }
+
+      if (
+        routeEvent instanceof NavigationEnd ||
+        routeEvent instanceof NavigationError ||
+        routeEvent instanceof NavigationCancel
+      ) {
+        this.showLoadingIndicator = false;
+      }
+    });
+  }
+
+  public onPageNotFound(): boolean {
+    if (this.router.url.includes('/404')) {
+      this.isPageNotFound = false;
+    } else {
+      this.isPageNotFound = true;
+    }
+    return this.isPageNotFound;
+  }
 }
