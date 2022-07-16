28582013412de122e292e873952aa8f06a20022a
Mulyono Putra <mulyonoputra27@outlook.com>
2022-07-16 21:48:05 +0700

    Move folder

--- src/app/components/atoms/error-messages-form/error-messages-form.component.ts
+++ src/app/components/atoms/error-messages-form/error-messages-form.component.ts
@@ -3,13 +3,12 @@ import { UntypedFormControl } from '@angular/forms';
 import { ValidationService } from 'src/app/core/service/validation.service';
 
 @Component({
-  selector: 'app-error-messages-form',
+  selector: 'error-messages',
   templateUrl: './error-messages-form.component.html',
   styleUrls: ['./error-messages-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorMessagesFormComponent implements OnInit {
-
   @Input() control!: UntypedFormControl;
 
   constructor() { }
@@ -18,8 +17,8 @@ export class ErrorMessagesFormComponent implements OnInit {
   }
 
   get errorMessage() {
-    for (let propertyName in this.control.errors) {
-      if (this.control.errors.hasOwnProperty(propertyName) && this.control.touched) {
+    for (let propertyName in this.control?.errors) {
+      if (this.control.errors.hasOwnProperty(propertyName) && this.control?.touched) {
         return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
       }
     }
