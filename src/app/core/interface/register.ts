58dd25c9daf3eceeb46aa2733c92ea28594c1334
Mulyono Putra <mulyonoputra27@outlook.com>
2022-07-16 20:01:42 +0700

    Update register.ts

--- src/app/core/interface/register.ts
+++ src/app/core/interface/register.ts
@@ -1,6 +1,14 @@
 export interface Register {
-  username!: string
-  email!: string
-  password!: string
-  confirmPassword!: string
+  username: string
+  email: string
+  password: string
+  confirmPassword: string
 }
+
+export interface RegisterPhoneNumber {
+  username: string
+  phone: string
+  password: string
+  confirmPassword: string
+}
+
