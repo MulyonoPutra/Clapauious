d76cb93b0c60abc4216e1b038172df52fe03d352
Mulyono Putra <mulyonoputra27@outlook.com>
2022-07-16 20:02:06 +0700

    Update Register Component

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Register, RegisterPhoneNumber } from 'src/app/core/interface/register';
+import { ValidationService } from 'src/app/core/service/validation.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../login/login.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  registerValue!: Register;
+  registerPhoneNumberValue!: RegisterPhoneNumber
+
+  emailForm!: FormGroup;
+  phoneNumberForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initEmailForms();
+    this.initPhoneNumberForms();
   }
+
+  initEmailForms(): void {
+    this.emailForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, ValidationService.emailValidator]],
+      password: ['', [Validators.required, ValidationService.passwordValidator]],
+      confirmPassword: ['', Validators.required]
+    },
+      {
+        validator: ValidationService.confirmPasswordValidator('password', 'confirmPassword')
+      }
+    )
+  }
+
+  initPhoneNumberForms(): void {
+    this.phoneNumberForm = this.fb.group({
+      username: ['', Validators.required],
+      phone: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    })
+  }
+
+  registerWithEmail(form: any): void {
+    this.registerValue = form.value;
+    console.log(`register with Email` + this.registerValue);
+  }
+
+  registerWithPhoneNumber(form: any): void {
+    this.registerPhoneNumberValue = form.value;
+    console.log(`register with Phone` + this.registerPhoneNumberValue);
+  }
+
+  disabled(event: any): void {
+    console.log(`disabled: ${event}`);
+  }
+
+
+
 
 }
--- src/app/modules/authentication/register/register.module.ts
+++ src/app/modules/authentication/register/register.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import { MaterialModule } from 'src/app/shared/material.module';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +15,10 @@ import { MaterialModule } from 'src/app/shared/material.module';
   imports: [
     CommonModule,
     RegisterRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class RegisterModule { }
